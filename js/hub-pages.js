ec41504b9aac38c712d246414ce09d6d572e6ab4
m.trebilcock <mike.trebilcock@cornwall.ac.uk>
2015-03-05 13:26:19 +0000

    Convert career page to ajax load

--- js/hub-pages.js
+++ js/hub-pages.js
@@ -1,28 +1,28 @@
-(function($) {
-
+(function ($) {
+    "use strict";
 	/* ISOTOPE FOR PORTFOLIO ITEMS */
-	if ( $( "#career-grid" ).length ) {
-		var $container = $('#career-grid').imagesLoaded( function() {
+	if ($("#career-grid").length) {
+		var $container = $('#career-grid').imagesLoaded(function () {
 			var isotope = function () {
 				$container.isotope({
 					resizable: false,
-					itemSelector: '.entry',
+					itemSelector: '.entry'
 				});
-			};
+            };
 			isotope();
 		});
 
-		$('div.career-filter ul a').click(function(){
-			var selector = $(this).attr('data-filter');
+		$('div.career-filter ul a').click(function () {
+            var selector = $(this).attr('data-filter');
 			$('html, body').animate({
-        		scrollTop: $( '#career-grid-anchor' ).offset().top
-    		}, 500);
+                scrollTop: $('#career-grid-anchor').offset().top
+            }, 500);
 			$container.isotope({
 				filter: selector,
 				animationOptions: {
 					duration: 750,
 					easing: 'linear',
-					queue: false,
+					queue: false
 				}
 			});
 			return false;
@@ -30,15 +30,15 @@
 		
 		var $optionSets = $('div.career-filter ul'),
 			$optionLinks = $optionSets.find('a');
-		$optionLinks.click(function(){
+		$optionLinks.click(function () {
 			var $this = $(this);
 			// don't proceed if already selected
-			if ( $this.hasClass('selected') ) {
+			if ($this.hasClass('selected')) {
 				return false;
 			}
 			var $optionSet = $this.parents('div.career-filter ul');
 			$optionSet.find('.selected').removeClass('selected');
-			$this.addClass('selected'); 
+			$this.addClass('selected');
 		});
 
 		$container.isotope({
@@ -46,31 +46,30 @@
 			animationOptions: {
 				duration: 750,
 				easing: 'linear',
-				queue: false,
+				queue: false
 			}
 		});
-		
- 		$( ".career-popup" ).each(function(index){
-			var popupName = $(this).attr("id");						
-			$("[data-popup='"+popupName+"']" ).on('click',function() {
-				$('html, body').addClass('noscroll');
-				var popupName = 'engineering';//$(this).attr("data-popup");
-				var popup = document.getElementById(popupName);
-			 	classie.toggle( popup, 'cbp-spmenu-open' );
-                $ ("#close").addClass ('show-close');    
 
-				
-				$('.counter').counterUp({
-				   delay: 100,
-				   time: 3000
-			   });
-		 	});
-	   });
+
+        $("[data-popup]").on('click', function () {
+            $('html, body').addClass('noscroll');
+            var popupName = 'engineering';//$(this).attr("data-popup");
+            var popup = document.getElementById(popupName);
+            $.get('./career-pages/' + popupName + '.html', function (data) {
+                $("#career-content").html(data);
+                $('.career-popup').addClass('cbp-spmenu-open');
+                $("#close").addClass('show-close');
+                $('.counter').counterUp({
+                    delay: 100,
+                    time: 3000
+                });
+            });
+        });
 		
-		$(".career-help-bar > .ls-close" ).on('click',function() {
+		$(".career-help-bar > .ls-close").on('click', function () {
 			$('html, body').removeClass('noscroll');
 			$(this).parent().parent().removeClass('cbp-spmenu-open');
-            $ ("#close").removeClass ('show-close');
+            $("#close").removeClass('show-close');
 		});
-	}	
+	}
 })(jQuery);
