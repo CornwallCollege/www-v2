3ae8313e66eb322ea905ae795718d5cade1f2ec3
jbyfordrew <jeremybyfordrew@gmail.com>
2015-04-15 10:51:40 +0100

    undo alerts

--- js/BetterTube.js
+++ js/BetterTube.js
@@ -7,16 +7,13 @@ else {
 
 function replaceThumbnailWithIframe(e){
   e = e || window.event;
-  var target = e.target || e.srcElement;
-    alert('here1');
-  if (target.className.match(/BetterTube-playBtn/)){
-      alert('here2');
+  var target = e.target || e.srcElement;    
+  if (target.className.match(/BetterTube-playBtn/)){      
     parent = target.parentNode;
     id = parent.getAttribute('data-youtube-id');
     // w = parent.getAttribute('data-player-width');
     // h = parent.getAttribute('data-player-height');    
     inner = '<iframe class="youtubePlayer" src="https://www.youtube.com/embed/' + id + '?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1" frameborder="0"></iframe>';
-    parent.innerHTML = inner;
-      alert('here3');
+    parent.innerHTML = inner;      
   } 
 }
