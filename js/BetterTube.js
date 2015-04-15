62d84fe8923c54e2db6fba99976e6a55415c7c43
jbyfordrew <jeremybyfordrew@gmail.com>
2015-04-15 10:41:08 +0100

    try standard youtube player code

--- js/BetterTube.js
+++ js/BetterTube.js
@@ -12,9 +12,8 @@ function replaceThumbnailWithIframe(e){
     parent = target.parentNode;
     id = parent.getAttribute('data-youtube-id');
     // w = parent.getAttribute('data-player-width');
-    // h = parent.getAttribute('data-player-height');
-    // style = 'style="frameborder=0;"';
-    inner = '<iframe id="player" type="text/html" src="http://www.youtube.com/embed/' + id + '" frameborder="0" ></iframe>';
+    // h = parent.getAttribute('data-player-height');    
+    inner = '<iframe class="youtubePlayer" src="https://www.youtube.com/embed/' + id + '?autoplay=1&autohide=1&border=0&wmode=opaque&enablejsapi=1" frameborder="0"></iframe>';
     parent.innerHTML = inner;
   } 
 }
