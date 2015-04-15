534c50cbb68748ef5e8b7b56806e9fd8ef9e2763
jbyfordrew <jeremybyfordrew@gmail.com>
2015-04-15 11:27:46 +0100

    try to fix click BetterTube on iphone

--- js/BetterTube.js
+++ js/BetterTube.js
@@ -1,19 +1,16 @@
-if (document.body.addEventListener) {
-  document.body.addEventListener('click',replaceThumbnailWithIframe,false);
-}
-else {
-  document.body.attachEvent('onclick',replaceThumbnailWithIframe);//for IE
-}
+jQuery(document).ready(function ($) {
 
-function replaceThumbnailWithIframe(e){
-  e = e || window.event;
-  var target = e.target || e.srcElement;      
-  if (target.className.indexOf('BetterTube') > -1){      
-    parent = target.parentNode;
-    id = parent.getAttribute('data-youtube-id');
-    // w = parent.getAttribute('data-player-width');
-    // h = parent.getAttribute('data-player-height');    
-    inner = '<iframe class="youtubePlayer" src="https://www.youtube.com/embed/' + id + '?autoplay=1&autohide=1&border=0&wmode=opaque&rel=0&enablejsapi=1" frameborder="0"></iframe>';
-    parent.innerHTML = inner;      
-  } 
-}
+    replaceThumbnailWithIframe = function(e) {
+        parent = e.parentNode;
+        id = parent.getAttribute('data-youtube-id');
+        // w = parent.getAttribute('data-player-width');
+        // h = parent.getAttribute('data-player-height');    
+        inner = '<iframe class="youtubePlayer" src="https://www.youtube.com/embed/' + id + '?autoplay=1&autohide=1&border=0&wmode=opaque&rel=0&enablejsapi=1" frameborder="0"></iframe>';
+        parent.innerHTML = inner;
+    }
+
+    $('.BetterTube-playBtn').each(function (i, e) {
+        $(e).on('click', function() {replaceThumbnailWithIframe(e)});
+    });
+
+});
