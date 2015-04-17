901291871f25266b95cf9c3e4adcc109dead8211
jbyfordrew <jeremybyfordrew@gmail.com>
2015-04-17 11:50:32 +0100

    remove skrollr

--- js/cc-fixed-background.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {        
-    if (location.pathname.indexOf("skrollr") !== -1) {
-        window.s = skrollr.init();
-        skrollr.refresh = function() {
-            window.s.destroy();
-            window.s = skrollr.init();
-        };        
-    };
-});
