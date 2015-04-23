7e4d6406b10f5666cf93b344f9e0afa53516788a
jbyfordrew <jeremybyfordrew@gmail.com>
2015-04-23 11:10:12 +0100

    try fix full time hub page on lumia, thinking its the youtube include bettertube

--- js/BetterTube.js
+++ js/BetterTube.js
@@ -8,13 +8,13 @@ jQuery(document).ready(function ($) {
         // h = parent.getAttribute('data-player-height');    
         var autoplayBit = 1
         if (!autoplay) autoplayBit = 0;
-        inner = '<iframe class="youtubePlayer" src="https://www.youtube.com/embed/' + id + '?autoplay=' + autoplayBit + '&showinfo=0&autohide=1&border=0&wmode=opaque&rel=0&enablejsapi=1" frameborder="0"></iframe>';
+        inner = '<iframe class="youtubePlayer" src="https://www.youtube.com/embed/' + id + '?autoplay=' + autoplayBit + '&showinfo=0&autohide=1&border=0&wmode=opaque&rel=0&enablejsapi=1" frameborder="0" allowfullscreen></iframe>';
         parent.innerHTML = inner;
     }
 
     $('.BetterTube-playBtn').each(function (i, e) {
         //if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        if (/iPhone|iPad|iPod|IEMobile/i.test(navigator.userAgent)) {
             replaceThumbnailWithIframe(e, false);
         } else {
             $(e).on('click', function () {
