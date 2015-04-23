c5eef941c50b70961f7f238ba1ddf5624d7afe0b
jbyfordrew <jeremybyfordrew@gmail.com>
2015-04-23 10:31:40 +0100

    try fix full time hub page on lumia, thinking its the youtube include bettertube

--- js/BetterTube.js
+++ js/BetterTube.js
@@ -1,22 +1,28 @@
 jQuery(document).ready(function ($) {
 
-    replaceThumbnailWithIframe = function(e, autoplay) {
+    replaceThumbnailWithIframe = function (e, autoplay) {
         parent = e.parentNode;
         id = parent.getAttribute('data-youtube-id');
         // w = parent.getAttribute('data-player-width');
         // h = parent.getAttribute('data-player-height');    
         var autoplayBit = 1
         if (!autoplay) autoplayBit = 0;
-        inner = '<iframe class="youtubePlayer" src="https://www.youtube.com/embed/' + id + '?autoplay='+autoplayBit+'&showinfo=0&autohide=1&border=0&wmode=opaque&rel=0&enablejsapi=1" frameborder="0"></iframe>';
+        inner = '<iframe class="youtubePlayer" src="https://www.youtube.com/embed/' + id + '?autoplay=' + autoplayBit + '&showinfo=0&autohide=1&border=0&wmode=opaque&rel=0&enablejsapi=1" frameborder="0"></iframe>';
         parent.innerHTML = inner;
     }
 
     $('.BetterTube-playBtn').each(function (i, e) {
-        //if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        if( /iPhone|iPad|iPod|IEMobile/i.test(navigator.userAgent) ) {
-            replaceThumbnailWithIframe(e, false);
+        if (/IEMobile/i.test(navigator.userAgent)) {
+            //do nothing
         } else {
-        $(e).on('click', function() {replaceThumbnailWithIframe(e, true)});
+            //if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+                replaceThumbnailWithIframe(e, false);
+            } else {
+                $(e).on('click', function () {
+                    replaceThumbnailWithIframe(e, true)
+                });
+            }
         }
     });
 
