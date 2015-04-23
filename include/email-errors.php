de31fd854b729ba8a9a9eaf9a783175e8c146ec2
jbyfordrew <jeremybyfordrew@gmail.com>
2015-04-23 16:04:55 +0100

    email javascript errors to dev

--- include/email-errors.php
+++ include/email-errors.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && stripos($_SERVER['HTTP_REFERER'],'m.cornwall.ac.uk') != false ) {
-    $send = mail('mtest@cornwall.ac.uk', 'm.cornwall.ac.uk javascript error occured', print_r($_POST), 'From: mtest@cornwall.ac.uk' );
+    $send = mail('mtest@cornwall.ac.uk', 'm.cornwall.ac.uk javascript error occured', json_encode($_POST), 'From: mtest@cornwall.ac.uk' );
     exit();
 }
 
