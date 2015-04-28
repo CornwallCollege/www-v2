7b10d13b17af023158b51c7e5786795176dd2627
jbyfordrew <jeremybyfordrew@gmail.com>
2015-04-28 15:08:59 +0100

    protect email errors from malicious intent

--- include/email-errors.php
+++ include/email-errors.php
@@ -1,8 +1,15 @@
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && stripos($_SERVER['HTTP_REFERER'],'m.cornwall.ac.uk') != false ) {
-    $send = mail('mtest@cornwall.ac.uk', 'm.cornwall.ac.uk javascript error occured', $_POST['error'], 'From: mtest@cornwall.ac.uk' );    
+    $send = mail('mtest@cornwall.ac.uk', 'm.cornwall.ac.uk javascript error occured', test_input($_POST['error']), 'From: mtest@cornwall.ac.uk' );    
     exit();
 }
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}  
 
 ?>
