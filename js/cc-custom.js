389b6656289dd492a37b8657fb8b011969d3e587
jbyfordrew <jeremybyfordrew@gmail.com>
2015-05-14 14:44:37 +0100

    undo scale

--- js/cc-custom.js
+++ js/cc-custom.js
@@ -246,10 +246,10 @@
        
        $(function () {
            window.restoreZoomAndScrollTo = function(id) {            
-                var scale = 'scale(1)';
-                document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
-                document.body.style.msTransform =   scale;       // IE 9
-                document.body.style.transform = scale;
+                //var scale = 'scale(1)';
+                //document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
+                //document.body.style.msTransform =   scale;       // IE 9
+                //document.body.style.transform = scale;
                 $('#'+id).blur();
                 window.smoothScrollTo(id);
                 return false;
