10cf79d4c666218fefc78fe09bdf1b1fbd4915f8
jbyfordrew <jeremybyfordrew@gmail.com>
2015-11-27 16:30:55 +0000

    added bat file to git push changes

---
+++ SupportCode/DotNet/Apprenticeships/GitPushChanges.bat
@@ -0,0 +1,5 @@
+git remote add octopus https://438ce2cdb5c8a73a45e84bb4ec63f28be3c60230@github.com/CornwallCollege/www-v2.git
+git config user.email "jay.byfordrew@cornwall.ac.uk"
+git config user.name "jay.byfordrew"
+git commit -m "updated NAS vacancies"
+git push octopus
