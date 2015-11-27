8a0062b6ba242cab520a91faaf82c567d1bed112
jbyfordrew <jeremybyfordrew@gmail.com>
2015-11-27 16:51:38 +0000

    bat changes

--- SupportCode/DotNet/Apprenticeships/GitPushChanges.bat
+++ SupportCode/DotNet/Apprenticeships/GitPushChanges.bat
@@ -1,5 +1,6 @@
-git remote add octopus https://438ce2cdb5c8a73a45e84bb4ec63f28be3c60230@github.com/CornwallCollege/www-v2.git
-git config user.email "jay.byfordrew@cornwall.ac.uk"
-git config user.name "jay.byfordrew"
-git commit -m "updated NAS vacancies"
-git push octopus
+call git remote add octopus https://438ce2cdb5c8a73a45e84bb4ec63f28be3c60230@github.com/CornwallCollege/www-v2.git
+call git config user.email "jay.byfordrew@cornwall.ac.uk"
+call git config user.name "jay.byfordrew"
+call git commit -m "updated NAS vacancies"
+call git push octopus
+
--- SupportCode/DotNet/Apprenticeships/UpdateNasData.bat
+++
@@ -1,9 +0,0 @@
-git pull origin octopus
-
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319/msbuild ./NasDataPull/NasDataPull.sln /p:Configuration=Release
-
-NasDataPull\NasDataPull\bin\Release\NasDataPull.exe ../../../_data
-
-git commit -a -m "updated NAS data"
-
-git push origin octopus
