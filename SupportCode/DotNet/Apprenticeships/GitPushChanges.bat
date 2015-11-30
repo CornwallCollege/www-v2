0d7122d532ab822445735cba5032aa7ba1f369da
jbyfordrew <jeremybyfordrew@gmail.com>
2015-11-30 09:04:30 +0000

    moved .bat contents into inline script in bamboo

--- SupportCode/DotNet/Apprenticeships/GitPushChanges.bat
+++
@@ -1,5 +0,0 @@
-call git remote add octopus https://438ce2cdb5c8a73a45e84bb4ec63f28be3c60230@github.com/CornwallCollege/www-v2.git
-call git config user.email "jay.byfordrew@cornwall.ac.uk"
-call git config user.name "jay.byfordrew"
-call git commit -a -m "updated NAS vacancies"
-call git push octopus
