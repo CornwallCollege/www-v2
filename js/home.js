1cbb1a08be0f0be2496425d2cb787ba8314901ea
matt-tipler <matt.tipler@cornwall.ac.uk>
2016-09-23 13:57:11 +0100

    added video

--- js/home.js
+++ js/home.js
@@ -292,7 +292,7 @@ $(function () {
                 var video_options = [
                     {
                         brand: 'cornwall',
-                        videos: [ 'wall-sketching',  'landscape', 'landscape-2', 'landscape-4', 'landscape-6', 'mines', 'stac'], 
+                        videos: ['door-crop', 'wall-sketching',  'landscape', 'landscape-2', 'landscape-4', 'landscape-6', 'mines', 'stac'], 
                         mobile_videos: ['mobile/landscape', 'mobile/landscape-2', 'mobile/landscape-4', 'mobile/landscape-6', 'mobile/mines', 'mobile/stac'], 
                     },
                     {
